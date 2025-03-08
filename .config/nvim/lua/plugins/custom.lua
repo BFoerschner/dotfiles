f9bf3b4488a44bac5ad93483b60396d5b74f9feb
Björn Förschner <1569796+BFoerschner@users.noreply.github.com>
2025-03-08 12:42:30 +0100

    ref(jqplayground): swap unmaintained jq extension with newer fork

--- .config/nvim/lua/plugins/custom.lua
+++ .config/nvim/lua/plugins/custom.lua
@@ -247,7 +247,12 @@ return {
       },
     },
   },
-  { "jrop/jq.nvim" },
+  {
+    "yochem/jq-playground.nvim",
+    opts = {
+      cmd = { "yq" },
+    },
+  },
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
