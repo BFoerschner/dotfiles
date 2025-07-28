52e8f7373e7aa1907f26f46e323fb23f7dac40e2
Björn Förschner <1569796+BFoerschner@users.noreply.github.com>
2025-07-28 01:16:04 +0000

    fix(nvim): silicon made usable again

--- .config/nvim/lua/plugins/nvim-silicon.lua
+++ .config/nvim/lua/plugins/nvim-silicon.lua
@@ -2,34 +2,40 @@ return {
   "michaelrommel/nvim-silicon",
   lazy = true,
   cmd = "Silicon",
+  keys = {
+    {
+      "<leader>sc",
+      function()
+        require("nvim-silicon").file()
+      end,
+      mode = "v",
+      desc = "Save code screenshot file, copy path to clipboard",
+    },
+    {
+      "<leader>sC",
+      function()
+        require("nvim-silicon").clip()
+      end,
+      mode = "v",
+      desc = "Copy code screenshot to clipboard",
+    },
+  },
   config = function()
     local wk = require("which-key")
     wk.add({
       mode = { "v" },
       { "<leader>s", group = "Silicon" },
-      {
-        "<leader>sc",
-        function()
-          require("nvim-silicon").file()
-        end,
-        desc = "Save code screenshot file, copy path to clipboard",
-      },
-      {
-        "<leader>sC",
-        function()
-          require("nvim-silicon").clip()
-        end,
-        desc = "Copy code screenshot to clipboard",
-      },
+    })
+
+    require("nvim-silicon").setup({
+      command = "silicon",
+      output = function()
+        local path = "./" .. os.date("!%Y-%m-%dT%H-%M-%SZ") .. "_code.png"
+        if vim.fn.has("clipboard") == 1 then
+          vim.fn.setreg("+", path)
+        end
+        return path
+      end,
     })
   end,
-  opts = {
-    output = function()
-      local path = "./" .. os.date("!%Y-%m-%dT%H-%M-%SZ") .. "_code.png"
-      if vim.fn.has("clipboard") == 1 then
-        vim.fn.setreg("+", path)
-      end
-      return path
-    end,
-  },
 }
