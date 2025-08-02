77986c33dd227673143b35ba57cdf6e14209e9cb
Björn Förschner <1569796+BFoerschner@users.noreply.github.com>
2025-08-02 03:08:07 +0200

    feat(nvim): set nowrap for markdown files

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,6 +2,13 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = false
+  end,
+})
+
 -- automatically open the trouble diagnostics window when there are errors
 -- local trouble_open = false
 --
