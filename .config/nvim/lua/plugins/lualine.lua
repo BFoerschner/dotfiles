ec74c91f2d1348241b20629b767178a0ac6fc133
Björn Förschner <1569796+BFoerschner@users.noreply.github.com>
2025-08-03 21:36:44 +0000

    refactor(nvim,tmux): nvim statusline back to bottom, tmux statusline back to top
    Moved lualine components from winbar to main sections with UIEnter event
    loading and removed duplicate statusline plugin file

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,28 +1,21 @@
 return {
   "nvim-lualine/lualine.nvim",
   -- enabled = false,
+  event = "UIEnter", -- Load after UI is ready but before user interaction
+  priority = 1000,
   config = function()
     require("lualine").setup({
       options = {
         theme = "moonfly",
       },
       sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
-      },
-      winbar = {
-        lualine_a = { "navic" },
+        lualine_a = { "filename" },
         lualine_b = { "diagnostics" },
-        lualine_c = { "filename" },
+        lualine_c = { "navic" },
         lualine_x = { "encoding", "fileformat", "filetype" },
         lualine_y = { "" },
         lualine_z = { "location" },
       },
     })
-    vim.opt.laststatus = 0
   end,
 }
