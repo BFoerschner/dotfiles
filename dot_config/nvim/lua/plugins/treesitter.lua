e038c8d9ec017e6a2003bd7db52f5ec38ba9a143
Björn Förschner <1569796+BFoerschner@users.noreply.github.com>
2025-12-08 16:03:40 +0000

    chore(nvim): suppress treesitter installation notifications

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -7,4 +7,19 @@ return {
     ensure_installed = { "all" },
     sync_install = true,
   },
+  config = function(_, opts)
+    -- Temporarily disable vim.notify for treesitter setup, hide that damn installation message
+    local original_notify = vim.notify
+    vim.notify = function(msg, ...)
+      if type(msg) == "string" and (msg:match("treesitter") or msg:match("languages? installed")) then
+        return
+      end
+      original_notify(msg, ...)
+    end
+
+    require("nvim-treesitter.configs").setup(opts)
+
+    -- Restore original notify
+    vim.notify = original_notify
+  end,
 }
